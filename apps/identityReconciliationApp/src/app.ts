2041f66f3c899f4fe5efaa85882e41ae55ad3ff6
ansumandas441 <ansuman441@gmail.com>
2024-06-12 03:26:01 +0530

    Removed view, as is redundant

--- apps/identityReconciliationApp/src/app.ts
+++ apps/identityReconciliationApp/src/app.ts
@@ -1,20 +1,10 @@
 import express, { json, Request, Response , NextFunction} from 'express';
 require('dotenv').config();
-import cookieParser from 'cookie-parser';
-const path = require('path');
-import { resolve } from 'path';
 import routes from './routes/router';
-import axios from 'axios';
-import prisma from './prismaClient';
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 // Error handling middleware
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -23,20 +13,7 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 });
 
 
-// Home route
-app.get('/views', async (req, res) => {
-    try {
-        // Fetch data from your API
-        const contacts = await prisma.contact.findMany();
-        res.render('index', { contacts });
-    } catch (error) {
-        console.error('Error fetching data from API:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-
-//Endpoints
+//Endpoints for having identify and some supporting routes for testing
 app.use('/', routes);
 
 export default app;
